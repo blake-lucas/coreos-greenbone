05475cf0c405d8c3ba4a17db62676fb50c8ffe39
Blake Lucas <92932337+blake-lucas@users.noreply.github.com>
2023-10-25 12:38:36 -0500

    moving to running containers instead

--- main/post-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ouex pipefail
-
-systemctl enable ucore-paths-provision.service
-systemctl enable rpm-ostreed-automatic.timer
-
-sed -i 's/#AutomaticUpdatePolicy.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
