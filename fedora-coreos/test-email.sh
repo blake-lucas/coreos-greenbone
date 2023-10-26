e578caee923a33f9e448a960ea0e0fc3cbff1760
Blake Lucas <92932337+blake-lucas@users.noreply.github.com>
2023-10-26 15:47:36 -0500

    remove sudo from test-email

--- fedora-coreos/test-email.sh
+++ fedora-coreos/test-email.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Recipient email address to test the email relay: " TEST_EMAIL
 read -p "From address for the sending account: " SMTP_EMAIL
-sudo docker exec greenbone-community-edition-gvmd-1 /bin/bash -c 'echo "This is a test email" | mail -s "SMTP Auth Relay Is Working" ${TEST_EMAIL} -a "FROM:${SMTP_EMAIL}"'
+docker exec greenbone-community-edition-gvmd-1 /bin/bash -c 'echo "This is a test email" | mail -s "SMTP Auth Relay Is Working" ${TEST_EMAIL} -a "FROM:${SMTP_EMAIL}"'
