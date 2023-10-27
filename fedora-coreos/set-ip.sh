9666d73f6be56f52d8e1da3341f55de8c1b48564
Blake Lucas <92932337+blake-lucas@users.noreply.github.com>
2023-10-27 09:40:29 -0500

    set ipv4 to manual after stuff has been set

--- fedora-coreos/set-ip.sh
+++ fedora-coreos/set-ip.sh
@@ -6,9 +6,9 @@ read -p "Enter IPv4 address (192.168.1.123/24): " ipv4
 read -p "Enter gateway (192.168.1.254): " gateway
 read -p "Enter DNS (1.1.1.1,8.8.8.8): " dns
 
-sudo nmcli con mod "$nic" ipv4.method manual
 sudo nmcli con mod "$nic" ipv4.addresses $ipv4
 sudo nmcli con mod "$nic" ipv4.gateway $gateway
 sudo nmcli con mod "$nic" ipv4.dns "$dns"
+sudo nmcli con mod "$nic" ipv4.method manual
 
 sudo nmcli con up "$nic"
