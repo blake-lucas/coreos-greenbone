24f79b6ff7ad2f1dff3d10f2bf07dd8c7b5fe66b
Blake Lucas <92932337+blake-lucas@users.noreply.github.com>
2023-10-27 10:53:19 -0500

    minor change on set-ip

--- fedora-coreos/set-ip.sh
+++ fedora-coreos/set-ip.sh
@@ -9,6 +9,5 @@ read -p "Enter DNS (1.1.1.1,8.8.8.8): " dns
 sudo nmcli con mod "$nic" ipv4.addresses $ipv4
 sudo nmcli con mod "$nic" ipv4.gateway $gateway
 sudo nmcli con mod "$nic" ipv4.dns "$dns"
-sudo nmcli con mod "$nic" ipv4.method manual
-
-sudo nmcli con up "$nic"
+sudo nmcli con up "$nic" # Bring new IP up now
+sudo nmcli con mod "$nic" ipv4.method manual # Disable DHCP for next reboot/nmcli up
